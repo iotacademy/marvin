33306baa071d16ea428432750ffc8c92160f646f
Michiel <michiel@iotacademy.nl>
2016-07-14 11:35:20 +0200

    Added Powering up Marvin

--- Software/MarvinMultiSerial.ino
+++ Software/MarvinMultiSerial.ino
@@ -22,11 +22,16 @@
 
 
 void setup() {
+  // power RN2483
+  pinMode(6, OUTPUT);
+  digitalWrite(6, HIGH);
+  
   // initialize both serial ports:
   Serial.begin(57600);
   Serial1.begin(57600);
   pinMode(5, OUTPUT);
   digitalWrite(5, HIGH);
+
 }
 
 void loop() {
