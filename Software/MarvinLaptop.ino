eb989390494223eee0df479b5d8c204ff30f5205
Michiel <michiel@iotacademy.nl>
2016-09-09 12:06:13 +0200

    Created MarvinLaptop
    Enter hexadecimal input through your serial as payload.

---
+++ Software/MarvinLaptop.ino
@@ -0,0 +1,164 @@
+/*
+MarvinLaptop
+
+Basic controls of IoT Academy Marvin LoRa Development board through your Laptop
+
+This version supports:
+	- Sending LoRa uplink messages using ABP that are given as input from the serial port on your laptop
+	- Blink three times when sending data
+  - Power control to RN2483 module
+
+Instructions:
+	- Get the latest version of the Arduino software
+	- In Arduino IDE select Arduino Leonardo and com port of your device
+	- Please adjust ABP adresses and key below to match yours
+*/
+// Port to assign the type of lora data (any port can be used between 1 and 223)
+int     set_port  = 1;
+
+// Some standard ports that depend on the layout of the Marvin
+long    defaultBaudRate = 57600;
+int     reset_port = 5;
+int     RN2483_power_port = 6;
+int     led_port = 13;
+
+
+//*** Set parameters here BEGIN ---->
+String  set_nwkskey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // insert 32 hex char string here
+String  set_appskey = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // insert 32 hex char string here
+String  set_devaddr = "xxxxxxxx"; // insert 8 char hex string here
+//*** <---- END Set parameters here
+
+// Some global items
+String reader = "";
+
+/*
+ * Setup() function is called when board is started. Marvin uses a serial connection to talk to your pc and a serial
+ * connection to talk to the RN2483, these are both initialized in seperate functions. Also some Arduino port are 
+ * initialized and a LED is called to blink when everything is done. 
+ */
+void setup() {
+  InitializeSerials(defaultBaudRate);
+  initializeRN2483(RN2483_power_port, reset_port);
+  pinMode(led_port, OUTPUT); // Initialize LED port  
+  blinky();
+  print_to_console("Payload?");
+}
+
+void loop() {
+  while (Serial.available()) {
+    delay(3);  //delay to allow buffer to fill 
+    if (Serial.available() >0) {
+      char c = Serial.read();  //gets one byte from serial buffer
+      reader += c; //makes the string readString
+    } 
+  }
+
+  if (reader.length() >0) {
+    reader.replace("\r","");
+    reader.replace("\n","");
+    print_to_console("Read: " + reader);
+    delay(1000);
+    send_LoRa_data(set_port, reader);
+    blinky();
+    read_data_from_LoRa_Mod();
+    reader = "";
+    delay(1000);
+    print_to_console("Payload?");
+  }
+}
+
+void InitializeSerials(long baudrate)
+{
+  Serial.begin(baudrate);
+  Serial1.begin(baudrate);
+  delay(1000);
+  print_to_console("Serial ports initialised");
+}
+
+void initializeRN2483(int pwr_port, int rst_port)
+{
+  //Enable power to the RN2483
+  pinMode(pwr_port, OUTPUT);
+  digitalWrite(pwr_port, HIGH);
+  print_to_console("RN2483 Powered up");
+  delay(1000);
+  
+  //Disable reset pin
+  pinMode(rst_port, OUTPUT);
+  digitalWrite(rst_port, HIGH);
+
+  //Configure LoRa module
+  send_LoRa_Command("sys reset");
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("mac set nwkskey " + set_nwkskey);
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("mac set appskey " + set_appskey);
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("mac set devaddr " + set_devaddr);
+  read_data_from_LoRa_Mod();
+
+  //For this commands some extra delay is needed.
+  send_LoRa_Command("mac set adr on");
+  //send_LoRa_Command("mac set dr 0"); //uncomment this line to fix the RN2483 on SF12 (dr=DataRate)
+  delay(1000);
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("mac save");
+  delay(1000);
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("mac join abp");
+  delay(1000);
+  read_data_from_LoRa_Mod();
+
+  send_LoRa_Command("radio set crc off");
+  delay(1000);
+  read_data_from_LoRa_Mod();
+
+}
+
+void print_to_console(String message)
+{
+  Serial.println(message);
+}
+
+void read_data_from_LoRa_Mod()
+{
+  if (Serial1.available()) {
+    String inByte = Serial1.readString();
+    Serial.println(inByte);
+  }
+
+}
+
+void send_LoRa_Command(String cmd)
+{
+  print_to_console("Now sending: " + cmd);
+  Serial1.println(cmd);
+  delay(500);
+}
+
+void send_LoRa_data(int tx_port, String rawdata)
+{
+  send_LoRa_Command("mac tx uncnf " + String(tx_port) + String(" ") + rawdata);
+}
+
+
+void blinky()
+{
+  digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);              // wait for a second
+  digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);              // wait for a second
+  digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(500);              // wait for a second
+  digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
+  delay(500);              // wait for a second
+
+}
+
+
