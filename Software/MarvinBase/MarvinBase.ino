60075cf7f4d208605e12e031fcb3a40348c6023d
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2017-08-03 10:38:46 +0200

    Changed spaced to tabs of Software/MarvinBase/MarvinBase.ino

--- Software/MarvinBase/MarvinBase.ino
+++ Software/MarvinBase/MarvinBase.ino
@@ -6,7 +6,7 @@ Basic controls of IoT Academy Marvin LoRa Development board.
 This version supports:
 	- Sending LoRa uplink messages using ABP 
 	- Blink three times when sending data
-  - Power control to RN2483 module
+	- Power control to RN2483 module
 
 Instructions:
 	- Get the latest version of the Arduino software
@@ -41,122 +41,122 @@ const int pinTempSensor = A3;     // Grove - Temperature Sensor connect to A3 - 
  * initialized and a LED is called to blink when everything is done. 
  */
 void setup() {
-  Serial.begin(defaultBaudRate);
-  Serial1.begin(defaultBaudRate);
-  InitializeSerials(defaultBaudRate);
-  initializeRN2483(RN2483_power_port, reset_port);
-  pinMode(led_port, OUTPUT); // Initialize LED port  
-  blinky();
+	Serial.begin(defaultBaudRate);
+	Serial1.begin(defaultBaudRate);
+	InitializeSerials(defaultBaudRate);
+	initializeRN2483(RN2483_power_port, reset_port);
+	pinMode(led_port, OUTPUT); // Initialize LED port  
+	blinky();
 }
 
 void loop() {
 
-   int a = analogRead(pinTempSensor);
+	 int a = analogRead(pinTempSensor);
 
-    float R = 1023.0/((float)a)-1.0;
-    R = 100000.0*R;
+	float R = 1023.0/((float)a)-1.0;
+	R = 100000.0*R;
 
-    float temperature=1.0/(log(R/100000.0)/B+1/298.15)-273.15;   //convert to temperature via datasheet ;
+	float temperature=1.0/(log(R/100000.0)/B+1/298.15)-273.15;   //convert to temperature via datasheet ;
 
-    int temp = (int)temperature;  //We cast the float value to an integer because the format for the RN2483 is Hexadecimal, meaning the . character is not included. You can also multiply the temperature by 100 to send the decimals in the payload
-    Serial.print("temperature = ");
-    Serial.println(temperature);
+	int temp = (int)temperature;  //We cast the float value to an integer because the format for the RN2483 is Hexadecimal, meaning the . character is not included. You can also multiply the temperature by 100 to send the decimals in the payload
+	Serial.print("temperature = ");
+	Serial.println(temperature);
 
-    delay(100);
-    
-  send_LoRa_data(set_port, String(temp));
-  blinky();
-  delay(1000);
-  read_data_from_LoRa_Mod();
-  delay(30000);
+	delay(100);
+		
+	send_LoRa_data(set_port, String(temp));
+	blinky();
+	delay(1000);
+	read_data_from_LoRa_Mod();
+	delay(30000);
 }
 
 void InitializeSerials(int baudrate)
 {
-  delay(1000);
-  print_to_console("Serial ports initialised");
+	delay(1000);
+	print_to_console("Serial ports initialised");
 }
 
 void initializeRN2483(int pwr_port, int rst_port)
 {
-  //Enable power to the RN2483
-  pinMode(pwr_port, OUTPUT);
-  digitalWrite(pwr_port, HIGH);
-  print_to_console("RN2483 Powered up");
-  delay(1000);
-  
-  //Disable reset pin
-  pinMode(rst_port, OUTPUT);
-  digitalWrite(rst_port, HIGH);
+	//Enable power to the RN2483
+	pinMode(pwr_port, OUTPUT);
+	digitalWrite(pwr_port, HIGH);
+	print_to_console("RN2483 Powered up");
+	delay(1000);
+	
+	//Disable reset pin
+	pinMode(rst_port, OUTPUT);
+	digitalWrite(rst_port, HIGH);
 
-  //Configure LoRa module
-  send_LoRa_Command("sys reset");
-  read_data_from_LoRa_Mod();
+	//Configure LoRa module
+	send_LoRa_Command("sys reset");
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("radio set crc off");
-  delay(1000);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("radio set crc off");
+	delay(1000);
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("mac set nwkskey " + set_nwkskey);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("mac set nwkskey " + set_nwkskey);
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("mac set appskey " + set_appskey);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("mac set appskey " + set_appskey);
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("mac set devaddr " + set_devaddr);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("mac set devaddr " + set_devaddr);
+	read_data_from_LoRa_Mod();
 
-  //For this commands some extra delay is needed.
-  send_LoRa_Command("mac set adr on");
-  delay(1000);
-  read_data_from_LoRa_Mod();
+	//For this commands some extra delay is needed.
+	send_LoRa_Command("mac set adr on");
+	delay(1000);
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("mac save");
-  delay(1000);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("mac save");
+	delay(1000);
+	read_data_from_LoRa_Mod();
 
-  send_LoRa_Command("mac join abp");
-  delay(1000);
-  read_data_from_LoRa_Mod();
+	send_LoRa_Command("mac join abp");
+	delay(1000);
+	read_data_from_LoRa_Mod();
 
 }
 
 void print_to_console(String message)
 {
-  Serial.println(message);
+	Serial.println(message);
 }
 
 void read_data_from_LoRa_Mod()
 {
-  if (Serial1.available()) {
-    String inByte = Serial1.readString();
-    Serial.println(inByte);
-  }
+	if (Serial1.available()) {
+		String inByte = Serial1.readString();
+		Serial.println(inByte);
+	}
 
 }
 
 void send_LoRa_Command(String cmd)
 {
-  print_to_console("Now sending: " + cmd);
-  Serial1.println(cmd);
-  delay(500);
+	print_to_console("Now sending: " + cmd);
+	Serial1.println(cmd);
+	delay(500);
 }
 
 void send_LoRa_data(int tx_port, String rawdata)
 {
-  send_LoRa_Command("mac tx uncnf " + String(tx_port) + String(" ") + rawdata);
+	send_LoRa_Command("mac tx uncnf " + String(tx_port) + String(" ") + rawdata);
 }
 
 
 void blinky()
 {
-  digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                     // wait for a second
-  digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                     // wait for a second
-  digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(500);                     // wait for a second
-  digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
-  delay(500);                     // wait for a second
+	digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(500);                     // wait for a second
+	digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
+	delay(500);                     // wait for a second
+	digitalWrite(led_port, HIGH);   // turn the LED on (HIGH is the voltage level)
+	delay(500);                     // wait for a second
+	digitalWrite(led_port, LOW);    // turn the LED off by making the voltage LOW
+	delay(500);                     // wait for a second
 
 }
